062735f0b79a93108b4b98dbc356be888b332a21
Darkhey <pr3kum@gmail.com>
2025-06-27 10:48:57 +0200

    Use env vars for Supabase config

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vzolmlztzewveszdykwq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6b2xtbHp0emV3dmVzemR5a3dxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NDU5MjYsImV4cCI6MjA2NjUyMTkyNn0.GxRPZkwFWxsmiG_QXAV0BOpaLNV5mtO6QdEDC5eYSFY'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
